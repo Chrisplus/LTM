d52cf1a2c118cfc681299f5c96c2393c0ff06812
Chris <chris@chris-PDCC.(none)>
2013-09-02 21:39:02 +0800

    under process

--- src/com/chrisplus/ltm/LogService.java
+++ src/com/chrisplus/ltm/LogService.java
@@ -1,6 +1,64 @@
+
 package com.chrisplus.ltm;
 
-/* This class implement the core service of logging of LTM*/
-public class LogService {
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
 
+import com.chrisplus.rootmanager.RootManager;
+
+/**
+ * @author Chris Jiang
+ */
+public class LogService extends Service {
+
+    private final static String TAG = LogService.class.getSimpleName();
+
+    private final Messenger messenger = new Messenger(new LTMHandler(getBaseContext()));
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "OnBind");
+
+        if (RootManager.getInstance().hasRooted() && RootManager.getInstance().grantPermission()) {
+            return messenger.getBinder();
+        } else {
+            return null;
+        }
+    }
+
+    private class LTMHandler extends Handler {
+
+        private Context context;
+
+        private LTMHandler(Context ctx) {
+            context = ctx;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            Log.d(TAG, "Get Message " + msg.toString());
+            super.handleMessage(msg);
+        }
+
+    }
+
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+    
+    
 }
---
+++ src/com/chrisplus/ltm/utils/Constants.java
@@ -0,0 +1,17 @@
+
+package com.chrisplus.ltm.utils;
+
+/**
+ * This class is used to record the constants for LTM project.
+ * 
+ * @author Chris Jiang
+ */
+public class Constants {
+
+    /* Passing Message between application and service */
+    public static int MSG_REG_CLIENT = 7301;
+    public static int MSG_UNREG_CLIENT = 7302;
+    public static int MSG_BROADCAST_LOG = 7303;
+    public static int MSG_UPDATE_NOTIFICATION = 7304;
+    public static int MSG_TOGGLE_FOREGROUND = 7305;
+}
