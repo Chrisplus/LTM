b6617c892c7214af482d566a611df8dab3590a29
Chris <chris@chris-PDCC.(none)>
2013-09-03 19:08:18 +0800

    ADD Notification Control Logic

--- src/com/chrisplus/ltm/LogService.java
+++ src/com/chrisplus/ltm/LogService.java
@@ -1,6 +1,9 @@
 
 package com.chrisplus.ltm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -8,8 +11,10 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.chrisplus.ltm.utils.Constants;
 import com.chrisplus.rootmanager.RootManager;
 
 /**
@@ -51,13 +56,49 @@ public class LogService extends Service {
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate");
+        addNotification();
         super.onCreate();
     }
 
     @Override
     public void onDestroy() {
         Log.d(TAG, "OnDestroy");
+        removeNotification();
         super.onDestroy();
+    }
+
+    private void updateNotification(boolean active) {
+        NotificationManager notiManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notiManager.notify(Constants.NOTIFICATION_ID, getNotification(active));
+    }
+
+    private void removeNotification() {
+        NotificationManager notiManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notiManager.cancel(Constants.NOTIFICATION_ID);
+    }
+
+    private void addNotification() {
+        updateNotification(false);
+    }
+
+    private Notification getNotification(boolean isActavied) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        if (isActavied) {
+            builder.setSmallIcon(R.drawable.notification_actived);
+        } else {
+            builder.setSmallIcon(R.drawable.notification_deactived);
+        }
+        builder.setContentTitle(getString(R.string.notification_title)).setContentText(
+                getString(R.string.notification_content)).setAutoCancel(false);
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.setFlags(
+                Intent.FLAG_ACTIVITY_SINGLE_TOP
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        builder.setContentIntent(contentIntent);
+        return builder.build();
     }
 
 }
--- src/com/chrisplus/ltm/utils/Constants.java
+++ src/com/chrisplus/ltm/utils/Constants.java
@@ -1,6 +1,8 @@
 
 package com.chrisplus.ltm.utils;
 
+import android.app.Application;
+
 /**
  * This class is used to record the constants for LTM project.
  * 
@@ -9,9 +11,12 @@ package com.chrisplus.ltm.utils;
 public class Constants {
 
     /* Passing Message between application and service */
-    public static int MSG_REG_CLIENT = 7301;
-    public static int MSG_UNREG_CLIENT = 7302;
-    public static int MSG_BROADCAST_LOG = 7303;
-    public static int MSG_UPDATE_NOTIFICATION = 7304;
-    public static int MSG_TOGGLE_FOREGROUND = 7305;
+    public final static int MSG_REG_CLIENT = 7301;
+    public final static int MSG_UNREG_CLIENT = 7302;
+    public final static int MSG_BROADCAST_LOG = 7303;
+    public final static int MSG_UPDATE_NOTIFICATION = 7304;
+    public final static int MSG_TOGGLE_FOREGROUND = 7305;
+
+    /* Notification ID */
+    public final static int NOTIFICATION_ID = Application.class.getName().hashCode();
 }
